ee01634d3dc03cb52b19a143358480acd52e9a93
Aniss <aniss.sej@gmail.com>
2025-08-02 17:39:59 +0100

    Adds file-based routing with react-router and vite-plugin-pages
    Integrates automatic page routing using vite-plugin-pages and react-router, enabling scalable and maintainable navigation.
    
    Replaces manual route setup with generated routes for improved developer experience and future extensibility.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import routes from 'virtual:generated-pages-react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(routes)
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    Pages({
+      dirs: 'src/pages', // dossier source
+      extensions: ['jsx', 'tsx'],
+    })
   ],
 })
