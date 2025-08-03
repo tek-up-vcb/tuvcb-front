321b2fa2bcfdd738601b1734765903b3eb89c0ca
Aniss <aniss.sej@gmail.com>
2025-08-04 00:48:22 +0100

    route patched

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import routes from 'virtual:generated-pages-react'
+import './index.css'
 
 const router = createBrowserRouter(routes)
 
@@ -7,4 +10,8 @@ function App() {
   return <RouterProvider router={router} />
 }
 
-export default App
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
