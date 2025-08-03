130200a0972501098b0a7cfcaf7da0477f799877
Aniss <aniss.sej@gmail.com>
2025-08-03 23:47:33 +0100

    waiting for shadcn

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import Pages from 'vite-plugin-pages'
+import path from "path"
 
 
 // https://vite.dev/config/
@@ -12,6 +13,11 @@ export default defineConfig({
     Pages({
       dirs: 'src/pages', // dossier source
       extensions: ['jsx', 'tsx'],
-    })
+    }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
